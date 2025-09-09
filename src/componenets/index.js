c46125cb94aaec9a1a08281028a78b477781d98b
232Ahmed232 <x99aliahmed@gmail.com>
2025-09-09 10:34:45 +0500

    GOT IT

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -3,12 +3,14 @@ import Footer from "./Footer/Footer";
 import container from "./container/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
-
+import Login from "./Login";
+import Input from "./Input"
 export  {
     Header, 
     Footer,
     container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input
     
 }
